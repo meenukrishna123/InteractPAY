fecbe9b0dc3ca17e7ed1c6fa5ff5d278c21f38b8
Akshaya Sreekumar <akshayasreekumar@Viveks-Mac-mini.domain.name>
2022-01-28 18:06:26 +0530

    UI changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  console.log('I was triggered during render')
+  console.log("I was triggered during render");
+  onloadeddata();
+  //onLoad= {() => onloadeddata()}
   return (
-    <div className="App">
-      <header className="App-header">
-        {/*<img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hello World........!
-        </p>*/}
-        <h3 className="Interactpay">InterACT Pay</h3>
-      <h6>Your payment solution</h6>
-      </header>
+    <div className="App" >
+      <nav class="navbar navbar-expand-lg navbar-dark  Interactpay" >
+        <div class="container">
+        <a class="navbar-brand" href="#"><i class="fa fa-info-circle" aria-hidden="true"></i>
+          InterACT Pay
+        </a>
+        </div>
+      </nav>
+      <div class="container">
+      <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-1">Hello</div>
+        <div class="col-lg-6 col-md-6 col-sm-1">
+          <ul class="list-group">
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+              Cras justo odio
+              <span class="badge badge-primary badge-pill">14</span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+              Dapibus ac facilisis in
+              <span class="badge badge-primary badge-pill">2</span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+              Morbi leo risus
+              <span class="badge badge-primary badge-pill">1</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
     </div>
   );
+  
+  
+}
+function onloadeddata(){
+  console.log("I was loaded");
 }
 
 export default App;
